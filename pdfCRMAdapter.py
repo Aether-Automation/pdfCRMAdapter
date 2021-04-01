7c5d50cb4bdc3a0c445ec731a380c8a778797341
Dylan Griffiths <dylan.g@usask.ca>
2021-04-01 07:12:10 -0700

    Finalize logic.

--- pdfCRMAdapter.py
+++ pdfCRMAdapter.py
@@ -16,10 +16,9 @@ DONE - -> CRM kicks-off process from 'Generate Quote' button action.
 DONE - -> Images, details are spooled.  Images are accessible online, details are used to fill in an HTML page.
 DONE - -> Call to endpoint occurs; the posted parameters are 'filename' 'htmltext' 'quoteId'
 DONE-> Endpoint forks a separate python method:
-    --> This method invokes curl to turn the HTML into a PDF, and then invokes curl to attach in CRM.
+DONE    --> This method invokes curl to turn the HTML into a PDF, and then invokes curl to attach in CRM.
 
 """
-
 
 # Phase 3 -- final, attach.
 def attachPdfToCrm(authToken, htmlText, quoteId, filename):
@@ -32,57 +31,25 @@ def attachPdfToCrm(authToken, htmlText, quoteId, filename):
             filename + '.pdf',
             'rb')
     }
-    print("filename => " + filename)
+    # print("filename => " + filename)
     response = requests.post(url=url, files=request_body, headers=headers)
-    if response is not None:
-        print("HTTP Status Code : " + str(response.status_code))
-        print(response.json())
+    # if response is not None:
+    #     print("HTTP Status Code : " + str(response.status_code))
+    #     print(response.json())
     return True
 
 # Phase 2 -- convert HTML to PDF.
 def convertHtmlToPdf(authToken, htmlText, quoteId, filename):
     # print("convertHtmlToPdf convertHtmlToPdf convertHtmlToPdf convertHtmlToPdf !!! ")
-    # print("Authorization header => " + authToken)
-    # print("Given QuoteID => " + quoteId)
-    # print("Given filename => " + filename)
-    # print("Given HTML => " + htmlText)
-
-    orgHtml = open(filename + '.html', "wb")
+    time.sleep(15)  # Ensures the files are copied first.
+    orgHtml = open(filename + '.html', "w")
     htmlFilename = filename + '.html'
     orgHtml.write(htmlText)
     orgHtml.close()
-
-    # curl -v -X POST -d @htmlFilename -JLO http://138.197.166.196:5001/pdf?filename=filename.pdf
     result = subprocess.run(["curl", "-v", "-X", "POST", "-d",
                              "@" + htmlFilename, "-JLO",
                              "http://138.197.166.196:5001/pdf?filename=" + filename + ".pdf"])
-    print("Result => " + result)
-
-
-    # params = (
-    #     ('filename', 'result.pdf'),
-    # )
-    # data = open('attach_test_two.html', 'rb')
-    # response = requests.post('http://138.197.166.196:5001/pdf', params=params, data=data)
-    # if response is not None:
-    #     print("HTTP Status Code : " + str(response.status_code))
-    #     tempFile = open(filename + '.pdf', 'wb')
-    #     tempFile.write(response.content)
-    #     tempFile.close()
-
-
-    # url = 'http://138.197.166.196:5001/pdf?filename=' + filename + '.pdf'
-    # request_body = htmlText
-    # # print(type(request_body))
-    # response = requests.post(url=url, data=request_body.encode('ascii', 'ignore'))
-    # if response is not None:
-    #     print("HTTP Status Code : " + str(response.status_code))
-    #     tempFile = open(filename + '.pdf', "w")
-    #     tempFile.write(response.text)
-    #     tempFile.close()
-    #     attachPdfToCrm(authToken, htmlText, quoteId, filename)
-
-
+    attachPdfToCrm(authToken, htmlText, quoteId, filename)
     return True
 
 
@@ -94,13 +61,12 @@ def index():
     # print("Given QuoteID => " + request.form["quoteId"])
     # print("Given filename => " + request.form["filename"])
     # print("Given HTML => " + request.form["htmltext"])
-
     authToken = request.headers['Authorization']
     quoteId = request.form["quoteId"]
     outputFilename = request.form["filename"]
+    # outputFilename = "attach_test_two"
     htmlText = request.form["htmltext"]
 
-    outputFilename = "attach_test_two"
 
     thread = multiprocessing.Process(target=convertHtmlToPdf, args=(authToken,htmlText,quoteId,outputFilename))
     thread.start()
