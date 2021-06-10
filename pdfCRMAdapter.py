517595e34a59c76e11594cc179c95eb390b9457a
Andrew <operations@aetherautomation.com>
2021-06-09 21:07:42 -0700

    creator stuff

--- pdfCRMAdapter.py
+++ pdfCRMAdapter.py
@@ -21,10 +21,33 @@ DONE-> Endpoint forks a separate python method:
 DONE    --> This method invokes curl to turn the HTML into a PDF, and then invokes curl to attach in CRM.
 
 """
+# Phase 4 -- final, attach.
+def attachPdfToCreator(authToken, htmlText, module, recordId, filename, fieldName,urlEndpoint):
+    url = urlEndpoint + module + '/' + recordId + '/' + fieldName + '/upload'
+    headers = {
+        'Authorization': authToken
+    }
+    request_body = {
+        'file': open(
+            filename + '.pdf',
+            'rb')
+    }
+    # print("filename => " + filename)
+    response = requests.post(url=url, files=request_body, headers=headers)
+    if response is not None:
+        print("HTTP Status Code : " + str(response.status_code))
+        print(response.json())
+        # Clobber the temp pdf file.
+        os.remove(filename + ".pdf")
+    return True
+
+
+
 
 # Phase 3 -- final, attach.
 def attachPdfToCrm(authToken, htmlText, module, recordId, filename):
     url = 'https://www.zohoapis.com/crm/v2/' + module + '/' + recordId + '/Attachments'
+
     headers = {
         'Authorization': authToken
     }
@@ -43,7 +66,7 @@ def attachPdfToCrm(authToken, htmlText, module, recordId, filename):
     return True
 
 # Phase 2 -- convert HTML to PDF.
-def convertHtmlToPdf(authToken, htmlText, module, recordId, filename):
+def convertHtmlToPdf(authToken, htmlText, module, recordId, filename, fieldName, urlEndpoint, appType):
     # print("convertHtmlToPdf convertHtmlToPdf convertHtmlToPdf convertHtmlToPdf !!! ")
     time.sleep(35)  # Ensures the files are copied first.
     htmlFilename = "tempspool/" + filename + '.html'
@@ -57,7 +80,10 @@ def convertHtmlToPdf(authToken, htmlText, module, recordId, filename):
     result = subprocess.run(["curl", "-v", "-X", "POST", "-d",
                              "@" + htmlFilename, "-JLO",
                              "http://138.197.166.196:5001/pdf?filename=" + filename + ".pdf"])
-    attachPdfToCrm(authToken, htmlText, module, recordId, filename)
+    if appType != 'Creator':
+        attachPdfToCrm(authToken, htmlText, module, recordId, filename)
+    if appType == 'Creator':
+        attachPdfToCreator(authToken, htmlText, module, recordId, filename, fieldName, urlEndpoint)
     os.remove(htmlFilename)
     return True
 
@@ -75,6 +101,9 @@ def index():
     module = request.form["module"]
     outputFilename = request.form["filename"]
     htmlText = request.form["htmltext"]
+    appType = request.form.get('appType')
+    fieldName = request.form.get('fieldName')
+    urlEndpoint = request.form.get('urlEndpoint')
 
     thread = multiprocessing.Process(target=convertHtmlToPdf, args=(authToken,htmlText,module,recordId,outputFilename))
     thread.start()
